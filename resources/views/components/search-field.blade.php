c26e189da3c26fdfec6ba8a3f07733fd40820cb2
Mate <matet2001@gmail.com>
2024-09-12 15:51:55 +0200

    Made search input preserve search text

--- resources/views/components/search-field.blade.php
+++ resources/views/components/search-field.blade.php
@@ -1,7 +1,9 @@
 <section class="text-center">
     <x-forms.form action="/search" class="mt-6">
-        <x-forms.input :label="false" name="q" placeholder="Egyiptom"/>
+        <!-- Set the value attribute to retain the search query -->
+        <x-forms.input :label="false" name="q" placeholder="Egyiptom" :value="request('q')"/>
 
         <x-primary-button class="text-lg px-6 py-3">Keresés</x-primary-button>
     </x-forms.form>
 </section>
+
