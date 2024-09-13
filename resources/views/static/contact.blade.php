f4a4fe0247525947782e283bc5c7a5d3a5dd14c6
Mate <matet2001@gmail.com>
2024-09-13 17:57:08 +0200

    Made contact page

--- resources/views/static/contact.blade.php
+++ resources/views/static/contact.blade.php
@@ -1,3 +1,11 @@
 <x-app-layout>
-    Contact
+    <div class="h-full">
+        <div class="mx-auto border border-white text-center leading-5 px-6 py-6 rounded-xl m-10 container">
+            <h1 class="text-3xl text-white text-center text-bold">Kapcsolat</h1>
+            <br>
+            <strong>
+                E-mail:  napszfera@gmail.com
+            </strong>
+        </div>
+    </div>
 </x-app-layout>
