ccdd37ee7341c56215d1430e812a34097033d202
matet2001 <matet2001@gmail.com>
2025-01-23 18:13:12 +0100

    dockerize project

--- database/migrations/2024_10_01_085243_create_orders_table.php
+++ database/migrations/2024_10_01_085243_create_orders_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id(); // Auto-incrementing ID for the order
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Foreign key to users table
-            $table->decimal('total', 10, 2); // Total amount for the order
-            $table->string('status')->default('pending'); // Order status, e.g., pending, completed, etc.
-            $table->timestamps(); // Laravel's default created_at and updated_at columns
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->decimal('total', 10, 2);
+            $table->string('status')->default('pending');
+            $table->timestamps();
         });
     }
 
